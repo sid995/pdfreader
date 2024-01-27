37ee740498f942685e073a753d828f27f19487e5
Siddharth Kundu <sk31395@gmail.com>
2024-01-27 18:23:34 -0500

    fix go to chart button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import FileUpload from "@/components/FileUpload";
 import { Positioning } from "@/components/layout/positioning";
 import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
 import { UserButton, auth } from "@clerk/nextjs";
-import { LogIn } from "lucide-react";
+import { eq } from "drizzle-orm";
+import { ArrowRight, LogIn } from "lucide-react";
 import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth()
   const isAuth = !!userId
+
+  let firstChat;
+  if (userId) {
+    firstChat = await db.select().from(chats).where(eq(chats.userId, userId));
+    if (firstChat) {
+      firstChat = firstChat[0];
+    }
+  }
 
   return (
     <Positioning>
@@ -19,9 +30,15 @@ export default async function Home() {
           />
         </div>
         <div className="flex mt-2">
-          {isAuth &&
-            <Button>Go to Chats</Button>
-          }
+          {isAuth && firstChat && (
+            <>
+              <Link href={`/chat/${firstChat.id}`}>
+                <Button>
+                  Go to Chats <ArrowRight className="ml-2" />
+                </Button>
+              </Link>
+            </>
+          )}
         </div>
         <p className="max-w-xl mt-2 text-lg text-slate-800">
           Join million of professionals to instantly get their answers from AI

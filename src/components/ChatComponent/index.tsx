a4dc780d9e58ca9c412ca7323926c5dc16ef38d9
Siddharth Kundu <sk31395@gmail.com>
2024-01-27 17:05:09 -0500

    added chat persistance

--- src/components/ChatComponent/index.tsx
+++ src/components/ChatComponent/index.tsx
@@ -5,15 +5,26 @@ import { useChat } from "ai/react"
 import { Button } from '../ui/button'
 import { Send } from 'lucide-react'
 import MessageList from '../MessageList'
+import { useQuery } from '@tanstack/react-query'
+import { Message } from 'ai'
+import axios from 'axios'
 
 type Props = { chatId: number }
 
 const ChatComponent = ({ chatId }: Props) => {
+  const { data, isLoading: queryFetchLoading } = useQuery({
+    queryKey: ["chat", chatId],
+    queryFn: async () => {
+      const response: any = await axios.post<Message>("/api/get-messages", { chatId })
+      return response.data
+    }
+  })
   const { input, handleInputChange, handleSubmit, messages, isLoading } = useChat({
     api: '/api/chat',
     body: {
       chatId
-    }
+    },
+    initialMessages: data || []
   })
 
   useEffect(() => {
@@ -27,16 +38,20 @@ const ChatComponent = ({ chatId }: Props) => {
   }, [messages])
 
   return (
-    <div className="relative max-h-screen h-full" id="message-container">
+    <div className="relative max-h-screen h-full flex flex-col" id="message-container">
       <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
         <h3 className="text-xl font-bold">Chat</h3>
       </div>
 
-      <MessageList messages={messages} isLoading={isLoading} />
+      <MessageList
+        messages={messages}
+        isLoading={isLoading}
+        queryFetchLoading={queryFetchLoading}
+      />
 
       <form
         onSubmit={handleSubmit}
-        className="absolute bottom-0 inset-x-0 px-2 py-4 bg-white flex"
+        className="sticky bottom-0 inset-x-0 px-2 py-4 bg-white flex"
       >
         <Input
           value={input}
